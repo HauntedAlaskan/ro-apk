dfb5018b667dd94548d316f9a1d786125e55458b
HauntedAlaskan <132791406+HauntedAlaskan@users.noreply.github.com>
2023-11-25 00:21:35 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="Ro-Apk",
+  description="ro-apk is a powerful Python library created by HauntedAlaskan, tailored for a range of functionalities within the Roblox ecosystem.",
+  version="1.0.0",
+  packages=["ro-apk"],
+  requires=[]
+)
